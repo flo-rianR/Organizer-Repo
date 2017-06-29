983bb0352263416f6c7eb2ef54be19fa324e3ba1
flo-rianR <florian_r@gmx.de>
2017-06-29 23:37:27 +0200

    Added DB Viewer

--- app/src/main/java/com/hswgt/florian/organizer/MainActivity.java
+++ app/src/main/java/com/hswgt/florian/organizer/MainActivity.java
@@ -3,6 +3,8 @@ package com.hswgt.florian.organizer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -10,6 +12,7 @@ import android.widget.Toast;
 import database.Entry;
 import database.MySQLiteHelper;
 
+import static android.R.attr.start;
 import static android.R.id.list;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +24,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         eDB = new MySQLiteHelper(this);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if(id == R.id.dbView)
+        {
+            final Intent i = new Intent(this, AndroidDatabaseManager.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void confirm(View view)
