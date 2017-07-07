842e2239c1ae37b5c71fef89339505a0ffd0d6b4
Topol-P <tobias.polaczek@web.de>
2017-07-07 20:36:47 +0200

    Activity for Entry Details modified

--- app/src/main/java/com/hswgt/florian/organizer/EntryDetailActivity.java
+++ app/src/main/java/com/hswgt/florian/organizer/EntryDetailActivity.java
@@ -11,6 +11,8 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -43,6 +45,21 @@ public class EntryDetailActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_list, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
