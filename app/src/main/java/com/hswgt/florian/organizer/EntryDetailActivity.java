7ae91549e7ce12af2773bf8ca544fdf08fcb7eba
Topol-P <tobias.polaczek@web.de>
2017-07-07 20:32:12 +0200

    Activity for Entry Details modified

--- app/src/main/java/com/hswgt/florian/organizer/EntryDetailActivity.java
+++ app/src/main/java/com/hswgt/florian/organizer/EntryDetailActivity.java
@@ -1,13 +1,48 @@
 package com.hswgt.florian.organizer;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import database.MySQLiteHelper;
 
 public class EntryDetailActivity extends AppCompatActivity {
+
+    EditText edtName, edtPrice;
+    Button btnChange, btnUpdate ;
+    ImageView entryimage;
+
+    final int REQUEST_CODE_GALLERY = 999;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_detail);
+
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
     }
+
 }
